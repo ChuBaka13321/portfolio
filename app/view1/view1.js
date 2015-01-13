ed636173a93b267fba7706d72d33a31b78c01ef6
ChuBaka13321 <BrianChu13321@gmail.com>
2015-01-13 14:17:13 -0800

    added sunderp,ttt,firstwebsite

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope',function($scope) {
+
+$scope.projects = [
+    {'name': 'About Me',
+     'snippet': 'What I originally sent in my application.',
+     'age': 3,
+     'url': 'http://myfirstwebsite.bitballoon.com/',
+     'imageURL': 'view1/img/firstwebsite.png'},
+    {'name': 'Aliens TicTacToe',
+     'snippet': 'My first WDI project',
+     'age': 2,
+     'url': 'https://tictacchu.firebaseapp.com/',
+     'imageURL': 'view1/img/tttaliens.png'},
+    {'name': 'SunDerp\'s Adventure',
+     'snippet': 'A game we built together.',
+     'age': 1,
+     'url': 'http://sunderp.herokuapp.com/',
+     'imageURL': 'view1/img/sunDerp.png'}
+];
+
+$scope.orderProp = 'age';
 
 }]);
